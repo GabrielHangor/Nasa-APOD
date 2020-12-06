7ada1680adc8a8213eba738d8ff76d92f49b1b04
HangorG <gaba93@yandex.ru>
2020-12-06 17:23:54 +0300

    fetch data from the NASA API

---
+++ script.js
@@ -0,0 +1,17 @@
+// NASA API
+count = 10;
+const apirKey = "DEMO_KEY";
+const apiURL = `https://api.nasa.gov/planetary/apod?api_key=${apirKey}&count=${count}`;
+
+let resultsArray = [];
+
+// Get 10 images from NASA API
+async function getNasaPictures() {
+  try {
+    const response = await fetch(apiURL);
+    resultsArray = await response.json();
+  } catch (error) {}
+}
+
+// On Load
+getNasaPictures();
